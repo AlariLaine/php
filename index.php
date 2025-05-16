ffdc8120c8b49b91c03b4f35babcd5caaa28dee0
AlariLaine <144769581+AlariLaine@users.noreply.github.com>
2025-05-16 19:12:38 +0300

    nyyd teh

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="shortcut icon" href="img/favico.ico" />
+	<title>YLESANDED</title>
+	<meta name="keywords" content="HTML,CSS,HTML5,CSS3" />
+	<meta name="autor" content="Alari Laine IT-23" />
+	<meta charset="UTF-8" />
+</head>
+<body>
+  <main>
+      <header>
+          <h1 id="algusesse">YLESANDED</h1>
+         <p>6pime veebitehnoloogiat</p>
+     </header>
+      <nav>
+        <ul>
+            <li><a href="php1-3.php">Ylesanne 1-3</a></li>
+            <li><a href="php4.php">Ylesanne 4</a></li>
+            <li><a href="php5.php">Ylesanne 5</a></li>
+            <li><a href="php6.php">Ylesanne 6</a></li>
+            <li><a href="php7.php">Ylesanne 7</a></li>
+            <li><a href="php8.php">Ylesanne 8</a></li>
+            <li><a href="php9.php">Ylesanne 9</a></li>
+            <li><a href="php10">Ylesanne 10</a></li>
+            <li><a href="php12.php">Ylesanne 12</a></li>
+            <li><a href="php13.php">Ylesanne 13</a></li>
+            <li><a href="php14.php">Ylesanne 14</a></li>
+            <li><a href="KT">KT</a></li>
+         </ul>
+    </nav>    
+</body>
+</html>
